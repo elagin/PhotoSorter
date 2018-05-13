12fe5db15bafe8547d574430158f1b56a6071633
p.elagin <elagin.pasha@gmail.com>
2018-05-13 23:07:17 +0300

    Раскладываем файлы из указанной папки.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxProcessed = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(144, 168);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxProcessed
+            // 
+            this.textBoxProcessed.Location = new System.Drawing.Point(144, 215);
+            this.textBoxProcessed.Name = "textBoxProcessed";
+            this.textBoxProcessed.Size = new System.Drawing.Size(75, 20);
+            this.textBoxProcessed.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(225, 215);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBoxProcessed);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxProcessed;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,17 +3,64 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace PhotoSorter
 {
     public partial class Form1 : Form
     {
+        private int processedFiles = 0;
+        private Thread addDataRunner;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void AddDataThreadLoop()
+        {
+
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            //ThreadStart addDataThreadStart = new ThreadStart(AddDataThreadLoop);
+            //addDataRunner = new Thread(addDataThreadStart);
+            //addDataRunner.Start();
+
+            textBoxProcessed.Text = "0";
+            string inFolder = @"g:\DCIM\112D7000\";
+            string OufFolder = @"d:\фототест\";
+            string[] files = System.IO.Directory.GetFiles(inFolder);
+            foreach (string file in files)
+            {
+                int pos = file.LastIndexOf('\\');
+                string shortName = file.Substring(pos + 1, file.Length - pos - 1);
+                DateTime date = File.GetCreationTimeUtc(file);
+                string[] aaa = date.GetDateTimeFormats();
+           
+                string year = date.Year.ToString().Substring(2, 2);
+                string month = date.Month.ToString();
+                string day = date.Day.ToString();
+
+                //string newFolder = year + month + day;
+                string newFolder = aaa[46].Substring(2, aaa[46].IndexOf('T')-2);
+                newFolder = newFolder.Replace("-", String.Empty);
+
+                string newFullFolder = OufFolder + newFolder + "\\";
+                if (!Directory.Exists(newFullFolder))
+                {
+                    Directory.CreateDirectory(newFullFolder);
+                }
+                if (!File.Exists(newFullFolder + shortName))
+                    File.Copy(file, newFullFolder + shortName);
+                textBoxProcessed.Text = (++processedFiles).ToString();
+            }
+            //System.IO.Directory directory = new System.IO.Directory();
         }
     }
 }
